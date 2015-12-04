22c3705d942d543a95b6c0a0cb58b650088c6947
Kevin MacNeel <kevjac@gmail.com>
2015-12-04 15:19:02 -0800

    Initial Commit Introduction to F#

---
+++ HelloWorld/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let add x y = x + y
+
+let sub x y = x - y
+
+let printThreeNumbers num1 num2 num3 =
+    printfn "num1: %i" num1
+    printfn "num2: %i" num2
+    printfn "num3: %i" num3
+
+
+let SayHello() = 
+    printfn "Hello"
+
+
+[<EntryPoint>]
+let main argv = 
+    //printfn "Hello World"
+    //printThreeNumbers 5 (add 10 7) (sub 20 8)
+    //Console.ReadLine() |> ignore 
+    SayHello()
+    Console.ReadLine() |> ignore 
+    0
+     // return an integer exit code
+
+
+    
+
+    
---
+++ Library1/Library1.fs
@@ -0,0 +1,39 @@
+﻿
+#if INTERACTIVE
+#else 
+module JumpStart
+#endif
+
+// Alt + Enter to Run
+
+let x = 42
+let hi = "Hello"
+
+let Square x = x * x
+
+let Area (length :float) height=
+    length * height
+
+let Greeting name =
+    if System.String.IsNullOrWhiteSpace(name) then
+        "whoever you are"
+    else 
+        name
+
+let SayHiToMe me = 
+    printfn "Hi, %s" (Greeting me)
+
+let PrintNumbers min max =
+  let square x = x * x
+  for x in min..max do
+    printfn "%i %i" x (square x)
+
+
+let RandomPosition() = 
+    let r = new System.Random()
+    r.NextDouble(), r.NextDouble()
+
+let Latitude, Longitude = RandomPosition()
+
+open System.IO
+let files = Directory.EnumerateFiles("@c:\windows","*.exe")
